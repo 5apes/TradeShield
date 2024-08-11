3a8ef7764aa640df3daaccce83eed978ffff3c38
Lusapo Ngcwabe <lusapo@ngcwabe.co.za>
2024-08-11 22:11:30 +0200

    Initial thought process for Question 2

--- TechnicalAssessment-main/TS.TechnicalTest/DeepestPitAnswer.cs
+++ TechnicalAssessment-main/TS.TechnicalTest/DeepestPitAnswer.cs
@@ -4,6 +4,15 @@ public class DeepestPitAnswer
 {
     public static int Solution(int[] points)
     {
-        throw new NotImplementedException("Not completed yet");
+        // innitial depth and response if array is empty
+        int maxDepth = -1;
+
+        //looping from P = 0 to P = points.Length - 3, ensuring that there's room for Q and R within the array.
+        for (int P = 0; P < points.Length - 2; P++)
+        {
+
+        }
+
+        return maxDepth;
     }
 }
