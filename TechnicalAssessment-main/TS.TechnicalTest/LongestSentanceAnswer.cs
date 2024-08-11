d75d869638539ea00f3176501611818188206d93
Lusapo Ngcwabe <lusapo@ngcwabe.co.za>
2024-08-11 21:40:21 +0200

    Question 1 answer and downgrade to .Net 7

--- TechnicalAssessment-main/TS.TechnicalTest/LongestSentanceAnswer.cs
+++ TechnicalAssessment-main/TS.TechnicalTest/LongestSentanceAnswer.cs
@@ -5,6 +5,32 @@ public class LongestSentanceAnswer
 {
     public static int Solution(string s)
     {
-        throw new NotImplementedException("Not completed yet");
+        try
+        {
+            // split the string into sentences by '.', '!' and '?'.
+            string[] sentences = s.Split(new char[] { '.', '!', '?' }, StringSplitOptions.RemoveEmptyEntries);
+
+            // initialize max word count to 0.
+            int maxWordCount = 0;
+
+            foreach (var sentence in sentences)
+            {
+                // Split each sentence into words and count the number of valid words
+                var words = sentence.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                int wordCount = words.Count(word => word.Any(char.IsLetter));
+
+                // Update maxWordCount if current sentence has more words
+
+                if (wordCount > maxWordCount)
+                    maxWordCount = wordCount;
+            }
+
+            //return max word count
+            return maxWordCount;
+        }
+        catch(Exception ex)
+        {
+            throw (new Exception(ex.Message));
+        }
     }
 }
